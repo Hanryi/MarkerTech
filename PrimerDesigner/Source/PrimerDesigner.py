5b259f290aaa7934362bc39e49cfb8887f1943d4
9-34platform <154702913@qq.com>
2021-12-18 15:38:19 +0800

    v2.0

--- PrimerDesigner/Source/PrimerDesigner.py
+++ PrimerDesigner/Source/PrimerDesigner.py
@@ -1,21 +1,18 @@
 """
-Updated on December 09, 2021
+Updated on December 18, 2021
 
 @Author
 Han Rui (154702913@qq.com)
 
 @Usage
-> python3.x PrimerDesigner.py <i_int> <i_text> <i_str_1> <i_str_2> <<i_num_1>...<i_num_8>>
-# <i_int>: strategy num
-# <i_text>: readable text file. Seq and its SNP index interspersed by '\n'
-# <i_str_1>: user account path
-# <i_str_2>: filename of tag txt and xlsx is composed by username and time
-# <<i_num_1>...<i_num_8>>: 8 num get from default settings
+> python3 PrimerDesigner.py <i_int> <i_fa> <o_str> <i_num>(9)
+# <i_int>: strategy num (BP-Seq: 2; iBP-Seq: 3)
+# <i_fa>: readable text file
+# <o_str>: path of xlsx with suffix
+# <i_num>(9): 9 nums get from default settings
 
 @Function
-Containing 2 primer design strategies.
-    Each strategy will return the best choice from design result according
-    a series of params given by command line
+Design specific primers in batches for BP-Seq and iBP-Seq
 """
 
 import os
@@ -25,23 +22,22 @@ import primer3
 import pandas as pd
 
 
-def primer_designer(template_name, template_seq, keep_area, len_arr, melt_arr):
-    """
-    Designer for primers
+def primer_attr_matrix(temp_name, temp_seq, keep_area, len_arr, melt_arr):
+    """ Design primers and return main attr list from json format.
 
-    :param template_name: Str
+    :param temp_name: Str
         name of the template sequence
-    :param template_seq: Str
+    :param temp_seq: Str
         template sequence for designing primers
-    :param keep_area: Tuple
-        pair of index of snp in target_seq
+    :param keep_area: List
+        interval endpoints in temp_seq (len == 2)
     :param len_arr: List
-        containing lens of primers and PCR product
+        length args of primers and PCR product
     :param melt_arr: List
-        containing Tm value of primers
+        Tm args of primers
 
-    :return: Dict
-        containing all details of a best primer design strategy
+    :return: List
+        main attrs of one pair of primers
     """
     # Constant length value (118 = 150 - 32)
     left_primer_area_length = 150
@@ -49,22 +45,23 @@ def primer_designer(template_name, template_seq, keep_area, len_arr, melt_arr):
 
     # Select start index
     left_primer_start = max(
-        keep_area[0] - left_primer_area_length,
+        keep_area[1] - left_primer_area_length,
         0,
     )
     # right_primer_end = min()
     right_primer_start = min(
-        keep_area[1] - 2 + right_primer_area_length,
+        keep_area[0] - 2 + right_primer_area_length,
         len(tempSeq) - 1,
     )
+
     # Parameter configuration
     seq_args = {
         # Identify the source of the chosen primers (str)
-        'SEQUENCE_ID': template_name,
+        'SEQUENCE_ID': temp_name,
         # Target sequence (5'->3') and it cannot span several lines (str)
-        'SEQUENCE_TEMPLATE': template_seq,
+        'SEQUENCE_TEMPLATE': temp_seq,
         # Subset of template used to design primers ([int]; default: all)
-        'SEQUENCE_INCLUDED_REGION': [0, len(template_seq)],
+        'SEQUENCE_INCLUDED_REGION': [0, len(temp_seq)],
         # Internals that primer pairs must flank ([int]; default: None)
         'SEQUENCE_TARGET': [keep_area[0] - 1, keep_area[1] - keep_area[0]],
         # Primers interval which -1 means no limit ([[int],...]; default: None)
@@ -80,28 +77,34 @@ def primer_designer(template_name, template_seq, keep_area, len_arr, melt_arr):
         # Pick an internal primer (bool; default: 0)
         'PRIMER_PICK_INTERNAL_OLIGO': 0,
 
-        # ---Length
-        # Optimum length of primers (int; default: 20)
-        'PRIMER_OPT_SIZE': len_arr[0],
+        # ---Primer Length
         # Minimum length of primers (int; default: 18)
-        'PRIMER_MIN_SIZE': len_arr[1],
+        'PRIMER_MIN_SIZE': len_arr[0],
+        # Optimum length of primers (int; default: 20)
+        'PRIMER_OPT_SIZE': len_arr[1],
         # Maximum length of primers (int; default: 27)
         'PRIMER_MAX_SIZE': len_arr[2],
-        # Product size range list ([int]; default: 0)
-        'PRIMER_PRODUCT_SIZE_RANGE': [len_arr[3], len_arr[4]],
+
+        # ---Production Length (int, default: 0)
+        # Optimum length of production (int; default: 0)
+        'PRIMER_PRODUCT_OPT_SIZE': len_arr[4],
+        # Production size range list ([int]; default: 0)
+        'PRIMER_PRODUCT_SIZE_RANGE': [
+            [len_arr[3], len_arr[5]],
+        ],
 
         # ---Tm (Celsius)
-        # Optimum temperature of primers (float; default: 60.0)
-        'PRIMER_OPT_TM': melt_arr[0],
         # Minimum temperature of primers (float; default: 57.0)
-        'PRIMER_MIN_TM': melt_arr[1],
+        'PRIMER_MIN_TM': melt_arr[0],
+        # Optimum temperature of primers (float; default: 60.0)
+        'PRIMER_OPT_TM': melt_arr[1],
         # Maximum temperature of primers (float; default: 63.0)
         'PRIMER_MAX_TM': melt_arr[2],
         # Maximum difference between pair of Tm (float; default 100.0)
         'PRIMER_PAIR_MAX_DIFF_TM': 10,
 
         # Print secondary structures (boolean; default: 0)
-        'PRIMER_SECONDARY_STRUCTURE_ALIGNMENT': 1,
+        'PRIMER_SECONDARY_STRUCTURE_ALIGNMENT': 0,
 
         # ---GC Percent
         # Minimum GC percent of primers (float; default: 20.0)
@@ -117,31 +120,44 @@ def primer_designer(template_name, template_seq, keep_area, len_arr, melt_arr):
         # Maximum number of N in any primer (int; default: 0)
         'PRIMER_MAX_NS_ACCEPTED': 0,
     }
+    # Design primer
+    primer_attr = primer3.bindings.designPrimers(seq_args, global_args)
 
-    return primer3.bindings.designPrimers(seq_args, global_args)
+    # Organize main attr
+    main_attr = [
+        temp_name,
+        primer_attr['PRIMER_LEFT_0_SEQUENCE'],
+        primer_attr['PRIMER_RIGHT_0_SEQUENCE'],
+        primer_attr['PRIMER_LEFT_0'][0],
+        primer_attr['PRIMER_RIGHT_0'][0],
+        primer_attr['PRIMER_LEFT_0'][1],
+        primer_attr['PRIMER_RIGHT_0'][1],
+        round(primer_attr['PRIMER_LEFT_0_TM'], 2),
+        round(primer_attr['PRIMER_RIGHT_0_TM'], 2),
+        round(primer_attr['PRIMER_LEFT_0_GC_PERCENT'], 2),
+        round(primer_attr['PRIMER_RIGHT_0_GC_PERCENT'], 2),
+        primer_attr['PRIMER_PAIR_0_PRODUCT_SIZE'],
+    ]
+    # BRIDGE inherits from global variable
+    main_attr[2] = BRIDGE + main_attr[2]
 
+    return main_attr
 
-# Constant string
-BRIDGE = "ATAGCGACGCGTTTCAAC"
-
-# Args
-# strategy = eval(sys.argv[1])
-# filename = os.path.join(sys.argv[3], sys.argv[4])
-# lenArr = list(map(int, sys.argv[5: 10]))
-# meltArr = list(map(float, sys.argv[10: 13]))
-strategy = 3
-filename = 'testSeq(1)'
-lenArr = [20, 18, 25, 280, 320]
-meltArr = [60, 50, 65]
 
 # Template file
-# tempTxt = sys.argv[2]
-tempTxt = "testSeq(1).txt"
+tempTxt = sys.argv[2]
+attrTab = sys.argv[3]
+# Args
+strategy = eval(sys.argv[1])
+lenArr = list(map(int, sys.argv[4: 10]))
+meltArr = list(map(float, sys.argv[10:]))
+
+# Result of primer design
 primerAttrMat = []
 attrName = [
     'Name',
     'Primer (L)', 'Primer (R)',
-    '5`pos\n(L)', '5`pos\n(R)',
+    "5'pos\n(L)", "5'pos\n(R)",
     'Length\n(L)', 'Length\n(R)',
     'Tm (L)', 'Tm (R)',
     'GC (L)', 'GC (R)',
@@ -149,91 +165,86 @@ attrName = [
 ]
 primerAttrMat.append(attrName)
 
-with open(tempTxt) as tempFile:
-    # Init
+# Specific sequence in 5'pos
+BRIDGE = "ATAGCGACGCGTTTCAAC"
+sheetName = 'iBP-Primers'
+if strategy == 3:
+    BRIDGE += "NNNNNN"
+if strategy == 2:
+    sheetName = 'BP-Primers'
+
+# Init container globally
+tempName = ''
+keepArea = []
+tempSeq = ''
+
+tempFile = open(tempTxt)
+
+for line in tempFile:
+    if line[0] != '>':
+        """ Attach sequence behind valid header.
+        """
+        if tempName != '' and keepArea != [] and keepArea[0] <= keepArea[1]:
+            tempSeq += line.strip()
+        # Move to next line finally
+        continue
+
+    if len(tempSeq) >= 150 and len(tempSeq) >= keepArea[1]:
+        """ Design primer for previous valid record.
+        """
+        mainAttr = primer_attr_matrix(
+            tempName, tempSeq, keepArea, lenArr, meltArr
+        )
+        primerAttrMat.append(mainAttr)
+
+    # Init current container
     tempName = ''
-    tempSeq = ''
     keepArea = []
+    tempSeq = ''
 
-    for line in tempFile:
+    # Read current header
+    headerVec = line[1:].strip().split('|')
+    if len(headerVec) >= 2:
+        """ Sequence name.
+        """
+        tempName = headerVec[0].strip()
 
-        if line[0] == '>':
-            tempName = line[1:].strip()
-            tempSeq = ''
+        # Check the format of '-'
+        note = headerVec[1].replace(' ', '')
+        if note == '' or note.count('-') > 1 or '-' in [note[0], note[-1]]:
             continue
 
-        if line[0] == '#':
-            # Check for name and sequence length
-            if tempName == '' or len(tempSeq) < 150:
-                continue
+        """ Area annotation.
+        """
+        keepArea = [int(i) for i in note.split('-')]
 
-            # Check for area annotation
-            note = line[1:].strip()
-            if note == '':
-                continue
-            if note.count('-') > 1 or note[0] == '-' or note[-1] == '-':
-                continue
-            keepArea = [int(i) for i in line[1:].strip().split('-')]
+        # Expand single base interval makes all len == 2
+        if len(keepArea) == 1:
+            keepArea.append(keepArea[0])
 
-            # Expand single base
-            if keepArea[0] > len(tempSeq):
-                continue
-            if len(keepArea) == 1 and keepArea[0] < len(tempSeq):
-                keepArea.append(keepArea[0])
+tempFile.close()
 
-            # All len == 2
-            if keepArea[0] > keepArea[1] or keepArea[1] > len(tempSeq):
-                continue
+# Pick primer for the last sequence
+mainAttr = primer_attr_matrix(
+    tempName, tempSeq, keepArea, lenArr, meltArr
+)
+primerAttrMat.append(mainAttr)
 
-            # Primer design
-            # print(tempName, len(tempSeq), keepArea)
-            primerAttr = primer_designer(
-                tempName, tempSeq, keepArea, lenArr, meltArr
-            )
-            mainAttr = [
-                tempName,
-                primerAttr['PRIMER_LEFT_0_SEQUENCE'],
-                primerAttr['PRIMER_RIGHT_0_SEQUENCE'],
-                primerAttr['PRIMER_LEFT_0'][0],
-                primerAttr['PRIMER_RIGHT_0'][0],
-                primerAttr['PRIMER_LEFT_0'][1],
-                primerAttr['PRIMER_RIGHT_0'][1],
-                round(primerAttr['PRIMER_LEFT_0_TM'], 2),
-                round(primerAttr['PRIMER_RIGHT_0_TM'], 2),
-                round(primerAttr['PRIMER_LEFT_0_GC_PERCENT'], 2),
-                round(primerAttr['PRIMER_RIGHT_0_GC_PERCENT'], 2),
-                primerAttr['PRIMER_PAIR_0_PRODUCT_SIZE'],
-            ]
-
-            # The only difference
-            if strategy == 3:
-                mainAttr[2] = BRIDGE + "NNNNNN" + mainAttr[2]
-            if strategy == 2:
-                mainAttr[2] = BRIDGE + mainAttr[2]
-
-            primerAttrMat.append(mainAttr)
-            continue
-            # break
-
-        # Attach sequence string
-        tempSeq += line.strip()
-
-# Build dataframe
+# Export dataframe as excel
 primerAttrFrame = pd.DataFrame(primerAttrMat)
-
-# Export as excel
-frameWriter = pd.ExcelWriter(filename + '.xlsx', engine='xlsxwriter')
+frameWriter = pd.ExcelWriter(attrTab, engine='xlsxwriter')
 primerAttrFrame.to_excel(
-    frameWriter, sheet_name='Sheet1', header=False, index=False
+    frameWriter, sheet_name=sheetName, header=False, index=False
 )
 
 # # Print all column
 # pd.set_option('display.max_columns', None)
 # print(primerAttrFrame)
 
-# Customize style
+""" Customize style.
+"""
 primerBook = frameWriter.book
-primerSheet = frameWriter.sheets['Sheet1']
+primerSheet = frameWriter.sheets[sheetName]
 headerStyle = primerBook.add_format({
     'valign': 'vcenter',
     'align': 'center',
@@ -253,12 +264,19 @@ for row in range(1, primerAttrFrame.shape[0]):
     primerSheet.set_row(row, 16)
 # Set column style
 primerSheet.set_column('A:L', cell_format=defaultCellStyle)
+# Most chars in each column
+charLen = [0 for _ in range(6)]
+aimCol = [0, 1, 2, 3, 4, 11]
+for item in primerAttrMat[1:]:
+    for col in aimCol:
+        if len(str(item[col])) + 2 > charLen[aimCol.index(col)]:
+            charLen[aimCol.index(col)] = len(str(item[col])) + 2
 # Set column width (pixel)
-primerSheet.set_column('A:A', 7.64)
-primerSheet.set_column('B:B', 21.55)
-primerSheet.set_column('C:C', 47.45)
-primerSheet.set_column('D:E', 6.09)
-primerSheet.set_column('F:K', 7.18)
-primerSheet.set_column('L:L', 8.27)
+primerSheet.set_column('A:A', max(8.64, charLen[0]))
+primerSheet.set_column('B:B', max(21.64, charLen[1]))
+primerSheet.set_column('C:C', max(21.64, charLen[2]))
+primerSheet.set_column('D:E', max(7, charLen[3], charLen[4]))
+primerSheet.set_column('F:K', 7.64)
+primerSheet.set_column('L:L', max(9, charLen[5]))
 # Save as file
 frameWriter.save()
