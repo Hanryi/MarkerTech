833d03c9cf608ee9da4f9b6b9a466ad3d5a77d66
9-34platform <154702913@qq.com>
2022-03-06 21:51:03 +0800

    Sequence Calculate

--- iBP-Seq/AltFraction/Source/AltFraction.py
+++ iBP-Seq/AltFraction/Source/AltFraction.py
@@ -32,45 +32,141 @@ import pandas as pd
 import pysam
 
 
-def xlsx_reader(mut_file):
-    """ Read mutation args by line from a xlsx file.
+class MutMatrix:
+    """ Mutation Matrix class.
 
-    :param mut_file: (str)
-        xlsx filepath
-
-    :return: (2d list)
-        mutation matrix composed of strings
+    This class mainly provide a method to update mutation matrix which deals
+    with multi-mutation sites. In addition to updating the information matrix,
+    update_matrix() method also dynamically created class attributes for each
+    pair of multi-mutation sites.
     """
-    mut_mat = []
-    mut_book = opx.load_workbook(mut_file, read_only=True)
-    mut_sht = mut_book[mut_book.sheetnames[0]]
-    # read_only=True only allow rows attr but not columns attr
-    mut_row = mut_sht.rows
-    next(mut_row)
-    for row in mut_row:
-        mut_arr = []
-        for x in row:
-            mut_arr.append(str(x.value))
-        mut_mat.append(mut_arr)
-    return mut_mat
+    def __init__(self, mut_file: str):
+        self.path = mut_file
+        self.type = mut_file.split('.')[-1].lower()
+        self.overlapVec = set()
 
+    def sort_mutation(self):
+        """ Sort matrix by str of first col and int value of second col.
 
-def text_reader(mut_file):
-    """ Read mutation args by line from a text file.
+        :return: (2d list)
+            sorted matrix
+        """
+        raw_matrix = []
+        # TODO(154702913@qq.com): Add VCF format reader.
+        if self.type not in ["csv", "txt", "xlsx"]:
+            print("Invalid filetype")
+        if self.type == "xlsx":
+            raw_matrix = self.xlsx_reader()
+        if self.type == "csv" or self.type == "txt":
+            raw_matrix = self.text_reader()
+        return sorted(raw_matrix, key=lambda info: (info[0], eval(info[1])))
 
-    :param mut_file: (str)
-        path of csv or txt (separator=',')
+    def update_matrix(self):
+        """ Merge possible overlapping sites.
 
-    :return: (2d list)
-        mutation matrix composed of strings
-    """
-    mut_mat = []
-    mut_obj = open(mut_file)
-    next(mut_obj)
-    for x in mut_obj:
-        mut_mat.append(x.strip().split(','))
-    mut_obj.close()
-    return mut_mat
+        This class method scan the overlapping sites. For each pair of them,
+        the object attribute named self.overlapVec save their row index for
+        skipping them in subsequent iterations. Moreover, for each merged
+        vector, the REF and ALT sequence are been updated and the length of
+        union sequence is also been add to a new attribute which is dynamically
+        created.
+
+        :return: (2d list)
+            updated matrix, each row output a frequency table
+        """
+        update_mat = []
+        sorted_mat = self.sort_mutation()
+        for i in range(len(sorted_mat) - 1):
+            mut_size = len(sorted_mat[i][2])
+            # Scan overlapped mutation sites
+            if eval(sorted_mat[i][1]) + mut_size >= eval(sorted_mat[i + 1][1]):
+
+                # Add overlapped index to set
+                self.overlapVec = self.overlapVec | {i, i + 1}
+
+                union = self.merge_seq(
+                    sorted_mat[i][2], sorted_mat[i + 1][2],
+                    eval(sorted_mat[i][1]), eval(sorted_mat[i + 1][1])
+                )
+                merge_vec = [
+                    sorted_mat[i][0], sorted_mat[i][1],
+                    sorted_mat[i][3] + self.tail_seq(
+                        union, sorted_mat[i][2]
+                    ),
+                    self.head_seq(
+                        union, sorted_mat[i + 1][2]
+                    ) + sorted_mat[i + 1][3]
+                ]
+                # Update mutation vector and add attribute
+                update_mat.append(merge_vec)
+                setattr(self, "_".join(merge_vec), len(union))
+
+            if i not in self.overlapVec:
+                update_mat.append(sorted_mat[i])
+        # Update the last mutation vector
+        if len(sorted_mat) - 1 not in self.overlapVec:
+            update_mat.append(sorted_mat[-1])
+        return update_mat
+
+    @staticmethod
+    def merge_seq(seq_1, seq_2, pos_1, pos_2) -> str:
+        """ Merge two overlapping sequences.
+
+        :param seq_1: (str)
+            overlapping region is at the end of the seq_1
+        :param seq_2: (str)
+            overlapping region is at the head of the seq_2
+        :param pos_1: (int)
+            first base position of seq_1
+        :param pos_2: (int)
+            first base position of seq_2, and
+            pos_2 > pos_1
+
+        :return: (str)
+            merged sequence
+        """
+        return seq_1 + seq_2[len(seq_1) - (pos_2 - pos_1):]
+
+    @staticmethod
+    def head_seq(union_seq, seq_2):
+        return union_seq[: len(union_seq) - len(seq_2)]
+
+    @staticmethod
+    def tail_seq(union_seq, seq_1):
+        return union_seq[len(seq_1):]
+
+    def xlsx_reader(self):
+        """ Read mutation args by line from a xlsx file.
+
+        :return: (2d list)
+            mutation matrix composed of strings
+        """
+        mut_mat = []
+        mut_book = opx.load_workbook(self.path, read_only=True)
+        mut_sht = mut_book[mut_book.sheetnames[0]]
+        # read_only=True only allow rows attr but not columns attr
+        mut_row = mut_sht.rows
+        next(mut_row)
+        for row in mut_row:
+            mut_arr = []
+            for x in row:
+                mut_arr.append(str(x.value))
+            mut_mat.append(mut_arr)
+        return mut_mat
+
+    def text_reader(self):
+        """ Read mutation args by line from a text file (separator=',').
+
+        :return: (2d list)
+            mutation matrix composed of strings
+        """
+        mut_mat = []
+        mut_obj = open(self.path)
+        next(mut_obj)
+        for x in mut_obj:
+            mut_mat.append(x.strip().split(','))
+        mut_obj.close()
+        return mut_mat
 
 
 def umi_roster(umi_fq):
@@ -148,23 +244,15 @@ def mut_locator(seg_seq, leader_pos, mut_len, pos_arr):
 
 
 barcode = sys.argv[1]
-# TODO(154702913@qq.com): Allow overlap mutation site
 mutFile = sys.argv[2]
 bamDir = sys.argv[3]
 umiDir = sys.argv[4]
 # Output
 csvDir = sys.argv[5]
 
-# Read mutation file as matrix
-filetype = mutFile.split('.')[-1]
-mutMat = []
-# TODO(154702913@qq.com): Add VCF format reader.
-if filetype.lower() not in ["xlsx", "csv", "txt"]:
-    print("Invalid filetype")
-if filetype.lower() == "xlsx":
-    mutMat = xlsx_reader(mutFile)
-if filetype.lower() == "csv" or filetype.lower() == "txt":
-    mutMat = text_reader(mutFile)
+mutObj = MutMatrix(mutFile)
+# Read mutation table as updated matrix
+mutMat = mutObj.update_matrix()
 
 # Measure sample number by barcodes
 sample = 0
@@ -197,6 +285,9 @@ for spl in range(sample):
         vcfRef = mutArr[2]
         vcfAlt = mutArr[3]
         mutLen = max(len(vcfRef), len(vcfAlt))
+        # Numerical rewriting
+        if hasattr(mutObj, location):
+            mutLen = getattr(mutObj, location)
         # UMI container (Ref and Alt)
         umiPanel = [[], []]
 
